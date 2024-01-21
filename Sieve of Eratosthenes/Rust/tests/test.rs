7b79323041beddb4110b4f3eedbf38dde06bb59e
DarkLord76865 <mmima191@gmail.com>
2024-01-21 15:57:06 +0100

    add Sieve of Eratosthenes

---
+++ Sieve of Eratosthenes/Rust/tests/test.rs
@@ -0,0 +1,38 @@
+use sieve_of_eratosthenes::sieve_of_eratosthenes;
+
+
+
+#[test]
+fn primes_100() {
+    assert_eq!(sieve_of_eratosthenes(100).len(), 25);
+}
+
+#[test]
+fn primes_1000() {
+    assert_eq!(sieve_of_eratosthenes(1000).len(), 168);
+}
+
+#[test]
+fn primes_10000() {
+    assert_eq!(sieve_of_eratosthenes(10_000).len(), 1229);
+}
+
+#[test]
+fn primes_100000() {
+    assert_eq!(sieve_of_eratosthenes(100_000).len(), 9592);
+}
+
+#[test]
+fn primes_1000000() {
+    assert_eq!(sieve_of_eratosthenes(1_000_000).len(), 78498);
+}
+
+#[test]
+fn primes_10000000() {
+    assert_eq!(sieve_of_eratosthenes(10_000_000).len(), 664579);
+}
+
+#[test]
+fn primes_100000000() {
+    assert_eq!(sieve_of_eratosthenes(100_000_000).len(), 5761455);
+}
