55f493da6c995ade86591b133041522bcc45ca8f
Antonio Mamić <antoniomamic007@gmail.com>
2024-10-15 01:08:19 +0200

    improve test runner

--- test.py
+++ test.py
@@ -17,26 +17,48 @@ def main():
             print(f"    {chr(ord('a') + j)}) {language.ljust(10)} ... ", end="")
 
             match language:
-                case "Python":
-                    if not run_test(algorithm, language, ["ruff", "check", "."]):
+                case "C":
+                    if not run_test(os.path.join(algorithm, language), [
+                        "cmake",
+                        "-DCMAKE_C_COMPILER=gcc",
+                        "-DCMAKE_CXX_COMPILER=g++",
+                        "-DCMAKE_BUILD_TYPE=Release",
+                        "-DCMAKE_C_FLAGS='-Wall -Wextra -Wpedantic'",
+                        "-DCMAKE_CXX_FLAGS='-Wall -Wextra -Wpedantic'",
+                        "-G", "Ninja",
+                        "-S", ".",
+                        "-B", "build"
+                    ]):
                         failed += 1
                         continue
-                    if not run_test(algorithm, language, ["python", "-m", "unittest"]):
+                    if not run_test(os.path.join(algorithm, language), ["cmake", "--build", "build"]):
+                        failed += 1
+                        continue
+                    if not run_test(os.path.join(algorithm, language, "build"), ["ctest"]):
+                        failed += 1
+                        continue
+                    print("OK")
+                    succeeded += 1
+                case "Python":
+                    if not run_test(os.path.join(algorithm, language), ["ruff", "check", "."]):
+                        failed += 1
+                        continue
+                    if not run_test(os.path.join(algorithm, language), ["python", "-m", "unittest"]):
                         failed += 1
                         continue
                     print("OK")
                     succeeded += 1
                 case "Rust":
-                    if not run_test(algorithm, language, ["cargo", "clippy", "--", "-D", "warnings"]):
+                    if not run_test(os.path.join(algorithm, language), ["cargo", "clippy", "--", "-D", "warnings"]):
                         failed += 1
                         continue
-                    if not run_test(algorithm, language, ["cargo", "fmt", "--all", "--", "--check"]):
+                    if not run_test(os.path.join(algorithm, language), ["cargo", "fmt", "--all", "--", "--check"]):
                         failed += 1
                         continue
-                    if not run_test(algorithm, language, ["cargo", "build"]):
+                    if not run_test(os.path.join(algorithm, language), ["cargo", "build"]):
                         failed += 1
                         continue
-                    if not run_test(algorithm, language, ["cargo", "test"]):
+                    if not run_test(os.path.join(algorithm, language), ["cargo", "test"]):
                         failed += 1
                         continue
                     print("OK")
@@ -52,10 +74,10 @@ def main():
     if failed > 0:
         sys.exit(1)
 
-def run_test(algorithm, language, args) -> bool:
+def run_test(path, args) -> bool:
     ret = subprocess.run(
         args,
-        cwd=os.path.join(algorithm, language),
+        cwd=path,
         capture_output=True,
     )
     if ret.returncode != 0:
